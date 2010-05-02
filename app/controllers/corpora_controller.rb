8b972f7f558cf47891800cd8475630696212001a
GV <ganeshananthanarayanan@VaswanisMac.local>
2010-05-01 17:39:37 -0700

    adding get_corpora_stat function

--- app/controllers/corpora_controller.rb
+++ app/controllers/corpora_controller.rb
@@ -2,4 +2,10 @@ class CorporaController < ApplicationController
     def index
         @corpora = Corpus.find(:all)
     end
+    
+    def get_corpora_stat
+      @corpora = Corpus.find(:all)
+      result = @corpora.inject({}){|a,c| a[c] = c.docs.count}
+      render :json => result
+    end
 end
